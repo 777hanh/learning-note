0d275da5e3926b509e1317c12d740463610f7324
unknown <thanhthientran1511@gmail.com>
2024-06-04 00:36:21 +0700

    fix: change api

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -2,4 +2,5 @@ export const apiUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:5000/api'
         : //   'https://server-zeta-five-76.vercel.app/api'
+
           'https://server-zeta-five-76.vercel.app/api';
