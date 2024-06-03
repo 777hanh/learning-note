e18e2a6dc3b444d873727fe1d5e3693079df45c3
777hanh <47732070+777hanh@users.noreply.github.com>
2024-06-03 22:07:08 +0700

    Update constants.js

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,1 +1,1 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://192.168.1.7:5000/api' : 'https://dry-beach-88174.herokuapp.com/api'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://dry-beach-88174.herokuapp.com/api'
